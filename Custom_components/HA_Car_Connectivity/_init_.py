e0470ddaa7b26523a73cd3d1cd0118d6f4279d8f
raolss <44369522+raolss@users.noreply.github.com>
2026-03-09 21:08:17 +0100

    Add async_setup_entry for configuration entry
    Implement async setup for HA Car Connectivity integration

--- Custom_components/HA_Car_Connectivity/_init_.py
+++ Custom_components/HA_Car_Connectivity/_init_.py
@@ -1,1 +1,21 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
+from .coordinator import VWCoordinator
+from .const import DOMAIN
+
+PLATFORMS = ["sensor"]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+    coordinator = VWCoordinator(hass, entry.data)
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
