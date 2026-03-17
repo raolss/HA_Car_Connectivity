ef2957319cd75dc176dbbd10d5546ac0edd3d882
raolss <44369522+raolss@users.noreply.github.com>
2026-03-17 20:21:17 +0100

    Remove async_setup_entry and platform definitions
    Removed the async_setup_entry function and related platform setup.

--- custom_components/ha_car_connectivity/__init__.py
+++ custom_components/ha_car_connectivity/__init__.py
@@ -29,18 +29,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
 
-PLATFORMS = ["sensor"]
 
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-
-    coordinator = VWCoordinator(hass, entry.data)
-
-    await coordinator.async_config_entry_first_refresh()
-
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
-    return True
