d88c30b019013e5087820c3a6d306c34513e1c38
raolss <44369522+raolss@users.noreply.github.com>
2026-03-20 19:12:18 +0100

    Update __init__.py

--- custom_components/ha_car_connectivity/__init__.py
+++ custom_components/ha_car_connectivity/__init__.py
@@ -1,65 +1,18 @@
-import logging
-_LOGGER = logging.getLogger(__name__)
+#import logging
+#_LOGGER = logging.getLogger(__name__)
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.const import Platform
 
-#from .coordinator import VWCoordinator
 from .const import DOMAIN
-
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
-
-from carconnectivity.carconnectivity import CarConnectivity
-import carconnectivity_connectors.volkswagen
+from .api import CarConnectivityAPI
 
 PLATFORMS = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    #config = entry.data
-
-    config = {
-        "carConnectivity": {
-            "connectors": [
-                {
-                    "connectorId": "vw",
-                    "type": "volkswagen",
-                    "config": {
-                        "username": entry.data["username"],
-                        "password": entry.data["password"],
-                        "country": "SE", 
-                        "brand": "vW",
-                        "region": "emea",
-                    },
-                }
-            ]
-        }
-    }   
-
-    # Kör blocking init i executor
-    try:
-        cc = await hass.async_add_executor_job(
-            CarConnectivity,
-            config
-        )
-    
-    except Exception as e:
-        _LOGGER.error("CarConnectivity init failed: %s", e)
-        raise    
-
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = cc
-
-    return True
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
-
-async def async_setup_entry(hass, entry):
     api = CarConnectivityAPI(entry.data)
+
     vehicles = await api.get_vehicles()
 
     hass.data.setdefault(DOMAIN, {})
@@ -68,7 +21,14 @@ async def async_setup_entry(hass, entry):
         "vehicles": vehicles,
     }
 
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
